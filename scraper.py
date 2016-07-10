fc52db55ff54aaed0aa504b227f9209341610561
Megan Chen <mchen0000@gmail.com>
2016-07-10 00:10:54 -0400

    scraper update

--- scraper.py
+++ scraper.py
@@ -6,19 +6,31 @@ hillaryClinton = 'HillaryClinton'
 
 username = [donaldTrump, hillaryClinton]
 
-link = urllib2.urlopen('https://twitter.com/' + username[1])
 
-html = link.read()
+#un - Twitter handle
+def getTweet(un):
+    link = urllib2.urlopen('https://twitter.com/' + un)
 
-soup = BeautifulSoup(html, 'html.parser')
+    html = link.read()
 
-tweet_text = soup.find_all('div', 'js-tweet-text-container')
-tweet_timestamps = soup.find_all('a', 'tweet-timestamp')
-tweet_links = soup.find_all('a', 'js-details')
+    soup = BeautifulSoup(html, 'html.parser')
 
-for i in range(0, len(tweet_text)):
-    text = tweet_text[i].get_text().encode('ascii', 'ignore')
-    print(text)
-    #timestamp = datetime.datetime.strptime(tweet_timestamps[i]['data-original-title'], '%I:%M %p - %d %b %y')
-    print(tweet_timestamps[i]['title'])
-    #link = 'https://twitter.com' + tweet_links[i]['href']
+    tweet_text = soup.find_all('div', 'js-tweet-text-container')
+    tweet_timestamps = soup.find_all('a', 'tweet-timestamp')
+    tweet_links = soup.find_all('a', 'js-details')
+
+    numTweets = len(tweet_text)
+
+    text = [None] * numTweets
+    timestamp = [None] * numTweets
+
+    for i in range(0, numTweets):
+        text[i] = tweet_text[i].get_text().encode('ascii', 'ignore')
+        timestamp[i] = tweet_timestamps[i]['title']
+        #link = 'https://twitter.com' + tweet_links[i]['href']
+
+    return text, timestamp
+
+if __name__ == "__main__":
+    for un in username:
+        print(getTweet(un))
